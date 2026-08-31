aaa9fac3b9843687789ca167dc7a026eaf20e5fe
apmcshahpurabhitoni-gif <apmcshahpurabhitoni@gmail.com>
2026-08-31 16:43:04 +0530

    build: export canonical domain contracts

---
+++ src/mavis/domain/__init__.py
@@ -0,0 +1,20 @@
+from .models import (
+    BacktestResult,
+    Candle,
+    Freshness,
+    Instrument,
+    MarketSession,
+    MessageEvent,
+    PaperTrade,
+    Signal,
+    SignalResult,
+    StrategyInput,
+    TradePlan,
+    Warning,
+)
+
+__all__ = [
+    "BacktestResult", "Candle", "Freshness", "Instrument", "MarketSession",
+    "MessageEvent", "PaperTrade", "Signal", "SignalResult", "StrategyInput",
+    "TradePlan", "Warning",
+]
